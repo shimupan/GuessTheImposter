e58f437d9f47944af8af831b34687f320f03a9d5
shimupan <shimupan@gmail.com>
2024-04-04 19:19:36 -0400

    fixed profile pic changing and adding chat

--- src/web/src/components/UserProfile.tsx
+++ src/web/src/components/UserProfile.tsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from "react";
+
 export const UserProfile = ({username, leader, className}: {username: string, leader: boolean, className?: string}) => {
+    const [number, setNumber] = useState(0);
+    useEffect(() => {
+        if(!localStorage.getItem('number')) {
+            const num = Math.floor(Math.random() * (5 - 0 + 1)) + 0;
+            localStorage.setItem('number', num.toString());
+            setNumber(num);
+        } else {
+            setNumber(parseInt(localStorage.getItem('number') || '0'));
+        }
+    }, []);
     return (
         <div className={`max-w-md mx-auto bg-white rounded-lg shadow-md p-5 ${className}`}>
-            <img className="w-32 h-32 rounded-full mx-auto" src={`https://cdn.discordapp.com/embed/avatars/${Math.floor(Math.random() * (5 - 0 + 1)) + 0}.png`} alt="Profile picture" />
+            <img className="w-32 h-32 rounded-full mx-auto" src={`https://cdn.discordapp.com/embed/avatars/${number}.png`} alt="Profile picture" />
             <h2 className="text-center text-2xl font-semibold mt-3">{username}</h2>
             {leader && <p className="text-center text-sm text-gray-500">Leader</p>}
         </div>
--- src/web/src/pages/Room.tsx
+++ src/web/src/pages/Room.tsx
@@ -1,99 +1,145 @@
-import { useContext, useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import { SocketContext } from '../context/SocketContext';
-import { AuthContext } from '../context/AuthContext';
-import { Username } from '../components/Username';
-import { RoomNotFound } from '../components/RoomNotFound';
-import { UserProfile } from '../components/UserProfile';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { SocketContext } from "../context/SocketContext";
+import { AuthContext } from "../context/AuthContext";
+import { Username } from "../components/Username";
+import { RoomNotFound } from "../components/RoomNotFound";
+import { UserProfile } from "../components/UserProfile";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export const Room = () => {
-   const { roomId } = useParams();
-   const Socket = useContext(SocketContext);
-   const Auth = useContext(AuthContext);
-   const [room, setRoom] = useState(true);
-   const [users, setUsers] = useState<string[]>([]);
-   const [controls, setControls] = useState<string>("Start Game");
-   const [word, setWord] = useState<string>('Waiting for leader to start game...');
-   // Check for Login
-   if (!localStorage.getItem('username')) {
-      return <Username />;
-   } else {
-      Auth?.setUsername(localStorage.getItem('username') || '');
-   }
+  const { roomId } = useParams();
+  const Socket = useContext(SocketContext);
+  const Auth = useContext(AuthContext);
+  const [room, setRoom] = useState(true);
+  const [users, setUsers] = useState<string[]>([]);
+  const [controls, setControls] = useState<string>("Start Game");
+  const [word, setWord] = useState<string>(
+    "Waiting for leader to start game..."
+  );
 
-   // Join Socket Room
-   useEffect(() => {
-      if (roomId && Socket?.socket) {
-         console.log('Joining room', roomId);
-         Socket.socket.emit('join-room', roomId);
+  // Can be refactored into a separate component
+  const [currentMessage, setCurrentMessage] = useState("");
+  const [messages, setMessages] = useState<string[]>([]);
 
-         Socket.socket.on('room-not-found', () => {
-            setRoom(false);
-         });
+  // Check for Login
+  if (!localStorage.getItem("username")) {
+    return <Username />;
+  } else {
+    Auth?.setUsername(localStorage.getItem("username") || "");
+  }
 
-         Socket.socket.on('users-in-room', (users) => {
-            console.log('Users in room:', users[0]);
-            setUsers(users);
-         });
+  // Join Socket Room
+  useEffect(() => {
+    if (roomId && Socket?.socket) {
+      console.log("Joining room", roomId);
+      Socket.socket.emit("join-room", roomId);
 
-         // Listen for the 'category' event
-         Socket.socket.on('category', (category) => {
-            console.log('Category:', category);
-            setWord(category[0]);
-            toast("Your Role this Round is: " + category[1]);
-         });
+      Socket.socket.on("room-not-found", () => {
+        setRoom(false);
+      });
 
-         // Listen for the 'word' event
-         Socket.socket.on('word', (word) => {
-            console.log('Word:', word);
-            setWord(word[0]);
-            toast("Your Role this Round is: " + word[1]);
-            
-         });
-      }
-      return () => {
-         Socket?.socket?.off('room-not-found');
-         Socket?.socket?.off('users-in-room');
-         Socket?.socket?.off('category');
-         Socket?.socket?.off('word');
-      };
-   }, [roomId, Socket?.socket]);
+      Socket.socket.on("users-in-room", (users) => {
+        console.log("Users in room:", users[0]);
+        setUsers(users);
+      });
 
-   const handleGameState = () => {
-      setControls('Next Round');
-      Socket?.socket?.emit('load-word', roomId);
-   };
+      // Listen for the 'category' event
+      Socket.socket.on("category", (category) => {
+        console.log("Category:", category);
+        setWord(category[0]);
+        toast("Your Role this Round is: " + category[1]);
+      });
 
-   return room ? (
-      <>
-         <div className='bg-[#0077B6] h-screen w-screen md:flex'>
-            <div className="bg-[#03045E] relative h-3/5 w-full lg:w-3/4 flex flex-col justify-center items-center text-center rounded-br-xl">
-               <div className='w-1/2 max-w-lg bg-white rounded-lg py-32 text-center'>
-                  <h1>
-                     {word}
-                  </h1>
-               </div>
-               {users[0] === Auth?.username && (
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-12" onClick={handleGameState}>
-                     {controls}
-                  </button>
-               )}
+      // Listen for the 'word' event
+      Socket.socket.on("word", (word) => {
+        console.log("Word:", word);
+        setWord(word[0]);
+        toast("Your Role this Round is: " + word[1]);
+      });
+    }
+    return () => {
+      Socket?.socket?.off("room-not-found");
+      Socket?.socket?.off("users-in-room");
+      Socket?.socket?.off("category");
+      Socket?.socket?.off("word");
+    };
+  }, [roomId, Socket?.socket]);
+
+  const handleGameState = () => {
+    setControls("Next Round");
+    Socket?.socket?.emit("load-word", roomId);
+  };
+
+  // can be refactored into a separate component
+  const handleSendMessage = () => {
+    setMessages((prevMessages) => [...prevMessages, currentMessage]);
+    setCurrentMessage("");
+  };
+
+  return room ? (
+    <>
+      <div className="h-screen w-screen">
+        <div className="md:flex">
+          <div className="bg-[#03045E] h-3/5 w-full lg:w-3/4 flex flex-col justify-center items-center text-center rounded-br-xl py-28">
+            <div className="w-1/2 max-w-lg bg-white rounded-lg py-32 text-center">
+              <h1>{word}</h1>
             </div>
-            <div className='md:w-1/4 md:overflow-auto'>
-               {users.map((user) => {
-                  if(user === users[0]) {
-                     return <UserProfile key={user} username={user} leader={true} />;
-                  } else {
-                     return <UserProfile key={user} username={user} leader={false} className={"mt-5"}/>;
-                  }
-               })}
-            </div>
-            <ToastContainer position="top-center" />
-         </div>
-      </>
-   ) : (
-      <RoomNotFound />
-   );
+            {users[0] === Auth?.username && (
+              <button
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-12"
+                onClick={handleGameState}
+              >
+                {controls}
+              </button>
+            )}
+          </div>
+          <div className="md:w-1/4 md:overflow-auto">
+            {users.map((user) => {
+              if (user === users[0]) {
+                return <UserProfile key={user} username={user} leader={true} />;
+              } else {
+                return (
+                  <UserProfile
+                    key={user}
+                    username={user}
+                    leader={false}
+                    className={"mt-3"}
+                  />
+                );
+              }
+            })}
+          </div>
+        </div>
+
+        {/* Can be refactored into a separate component */}
+        <div className="w-3/4">
+          <div className="overflow-auto h-64 w-full mb-4 border rounded-lg p-3">
+            {messages.map((message, index) => (
+              <p key={index}>{message}</p>
+            ))}
+          </div>
+          <div className="flex">
+            <input
+              type="text"
+              value={currentMessage}
+              onChange={(e) => setCurrentMessage(e.target.value)}
+              className="flex-grow border rounded-lg p-2"
+              placeholder="Type a message..."
+            />
+            <button
+              onClick={handleSendMessage}
+              className="ml-2 px-4 py-2 bg-blue-500 text-white rounded-lg"
+            >
+              Send
+            </button>
+          </div>
+        </div>
+        <ToastContainer position="top-center" />
+      </div>
+    </>
+  ) : (
+    <RoomNotFound />
+  );
 };
